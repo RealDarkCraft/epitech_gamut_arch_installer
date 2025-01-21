11726078dad9601cf7cdd1871f8cf378b4846133
celestin <celestin@epitech.eu>
2025-01-21 11:08:33 +0100

    start2-3

--- src/install_arch.sh
+++ src/install_arch.sh
@@ -5,8 +5,22 @@ setfont ter-c24n
 echo "Setting France Timezone"
 timedatectl set-timezone Europe/Paris
 echo "Creating disk"
-yes | pvcreate /dev/sda
-yes | vgcreate my_vg /dev/sda
+
+echo -e "n\n\
+e\n\
+4\n\
++500M\n\
+t\n\
+ef\n\
+n\n\
+p\n\
+1\n\
++21G\n\
+w" | fdisk /dev/sda
+
+
+yes | pvcreate /dev/sda1
+yes | vgcreate my_vg /dev/sda1
 lvcreate -n root -L 15G my_vg
 lvcreate -n home -L 5G my_vg
 lvcreate -n boot -L 400M my_vg
@@ -14,15 +28,18 @@ lvcreate -n swap -L 500M my_vg
 mkfs.ext4 /dev/my_vg/root
 mkfs.ext4 /dev/my_vg/home
 mkfs.ext4 /dev/my_vg/boot
+mkfs.fat -F 32 /dev/sda4
 mkswap /dev/my_vg/swap
 echo "Mount disk"
 mount /dev/my_vg/root /mnt
 mkdir /mnt/home
 mount /dev/my_vg/home /mnt/home
+mkdir /mnt/esp
+mount /dev/sda4 /mnt/esp
 mkdir /mnt/boot
 mount /dev/my_vg/boot /mnt/boot
 swapon /dev/my_vg/swap
-
+yes | pacstrap -K /mnt base linux linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
 hwclock --systohc
@@ -33,8 +50,9 @@ mkinitcpio -P
 echo "root:admin" | chpasswd
 yes | pacman -S grub
 yes | pacman -S efibootmgr
-grub-install --target=i386-pc /dev/sda
+grub-install --target=x86_64-efi --efi-directory=esp --bootloader-id=GRUB --modules="lvm" --disable-shim-lock
 grub-mkconfig -o /mnt/boot/grub/grub.cfg
+yes | pacman -S sddm konsole plasma
 exit
 umount -R /mnt
 reboot
