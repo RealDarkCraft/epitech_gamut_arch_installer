c055d0f6d21561a7d44d16abf882c46dcc16c0dd
celestin <celestin@epitech.eu>
2025-01-21 11:44:08 +0100

    start2-7

--- src/install_arch.sh
+++ src/install_arch.sh
@@ -21,44 +21,6 @@ p\n\
 w" | fdisk /dev/sda
 
 
-yes | pvcreate /dev/sda1
-yes | vgcreate my_vg /dev/sda1
-lvcreate -n root -L 15G my_vg
-lvcreate -n home -L 5G my_vg
-lvcreate -n boot -L 400M my_vg
-lvcreate -n swap -L 500M my_vg
-mkfs.ext4 /dev/my_vg/root
-mkfs.ext4 /dev/my_vg/home
-mkfs.ext4 /dev/my_vg/boot
-mkfs.fat -F 32 /dev/sda4
-mkswap /dev/my_vg/swap
-echo "Mount disk"
-mount /dev/my_vg/root /mnt
-mkdir /mnt/home
-mount /dev/my_vg/home /mnt/home
-mkdir /mnt/esp
-mount /dev/sda4 /mnt/esp
-mkdir /mnt/boot
-mount /dev/my_vg/boot /mnt/boot
-swapon /dev/my_vg/swap
-yes | pacstrap -K /mnt base linux linux-firmware
-genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
-hwclock --systohc
-locale-gen
-echo "LANG=fr_FR.UTF-8" > etc/locale.conf
-echo "KEYMAP=fr" > /etc/vconsole.conf
-mkinitcpio -P
-echo "root:admin" | chpasswd
-yes | pacman -S grub
-yes | pacman -S efibootmgr
-grub-install --target=x86_64-efi --efi-directory=esp --bootloader-id=GRUB --modules="lvm" --disable-shim-lock
-grub-mkconfig -o /mnt/boot/grub/grub.cfg
-yes | pacman -S sddm konsole plasma
-exit
-umount -R /mnt
-reboot
-
 echo "Finished"
 
 
